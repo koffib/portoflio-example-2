ed971adff5ac1536dfd2654626ec0ff311c3fc43
koffib <kbecanty@student.rtc.edu>
2022-06-03 23:56:08 -0700

    updated

---
+++ script/js.js
@@ -0,0 +1,37 @@
+/*--=======Show SideBar========*/
+
+
+/*--======Sidebar Show====*/
+
+
+
+
+/*--======Sidebar======*/
+
+
+
+
+/*======Skills Tabs=====*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContent = document.querySelectorAll('[data-content]')
+
+
+      tabs.forEach(tab => {
+          tab.addEventListener("click", () => {
+              const target = document.querySelector(tab.dataset.target)
+
+              tabContent.forEach(tabContents => {
+                  tabContents.classList.remove('skills__active')
+              })
+
+              target.classList.add('skills__active')
+
+              
+              tabs.forEach(tab => {
+                tab.classList.remove('skills__active')
+            })
+
+            tab.classList.add('skills__active')
+
+          })
+      })
